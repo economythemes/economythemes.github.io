8e495f03192dc391c48aae356b6be73617c33c48
Zach <zbecker10@gmail.com>
2015-07-12 23:25:54 -0400

    Economy Themes

--- assets/et-main.js
+++ assets/et-main.js
@@ -1,3 +1,14 @@
 if (window.location.hostname != "localhost" && window.location.protocol != "https:") {
     window.location.href = "https:" + window.location.href.substring(window.location.protocol.length);
 }
+function autoResize(id){
+    console.log(id)
+    var newheight;
+    var newwidth;
+    if(document.getElementById){
+        newheight=document.getElementById(id).contentWindow.document .body.scrollHeight;
+        newwidth=document.getElementById(id).contentWindow.document .body.scrollWidth;
+    }
+    document.getElementById(id).height= (newheight) + "px";
+    // document.getElementById(id).width= (newwidth) + "px";
+}
